9f2d987c53773aa6e3a0a50ae24411a78c043bb2
Valerică Gembăşel <valericagembasel@yahoo.com>
2017-05-14 20:20:49 +0300

    Update sistem_expert.pl

--- sistem_expert.pl
+++ sistem_expert.pl
@@ -275,20 +275,27 @@ cum(Scop),
 cum_premise(X).
         
 interogheaza(Atr,Mesaj,[da,nu,nu_stiu,nu_conteaza],Istorie) :-
-!,write(Mesaj),nl,write('da nu nu_stiu nu_conteaza'),nl,
+!,write(Mesaj),nl,write('da nu nu_stiu nu_conteaza'),nl,write('fdfdfdf'),nl,
 de_la_utiliz(X,Istorie,[da,nu,nu_stiu,nu_conteaza]),
-det_val_fc(X,Val,FC),
-asserta( fapt(av(Atr,Val),FC,[utiliz]) ).
+det_val_fc(X,Val,FC),asserta( fapt(av(Atr,Val),FC,[utiliz]) ).
 
 interogheaza_mlt(Atr,Mesaj,Optiuni,Istorie) :-
 write(Mesaj),nl,append(Optiuni,[nu_stiu,nu_conteaza,gata],Optiuni1),
-citeste_opt(VLista,Optiuni1,Istorie),((VLista==[gata],!);
+citeste_opt(VLista,Optiuni1,Istorie),((VLista==[gata],!);(VLista==[nu_conteaza],nu_conteaza(Atr,Optiuni),!);
+(VLista=[nu_conteaza,fc,Val],nu_conteaza(Atr,Optiuni,Val),!);
 (assert_fapt(Atr,VLista),fail)).
 
 interogheaza(Atr,Mesaj,Optiuni,Istorie) :-
 write(Mesaj),nl,append(Optiuni,[nu_stiu,nu_conteaza],Optiuni1),
-citeste_opt(VLista,Optiuni1,Istorie),
-assert_fapt(Atr,VLista).
+citeste_opt(VLista,Optiuni1,Istorie),((VLista==[nu_conteaza],nu_conteaza(Atr,Optiuni),!);
+(VLista=[nu_conteaza,fc,Val],nu_conteaza(Atr,Optiuni,Val),!);
+assert_fapt(Atr,VLista)).
+
+nu_conteaza(Atr,[H],Val):- \+fapt(av(Atr,H),_,_),assert_fapt(Atr,[H,fc,Val]);true.
+nu_conteaza(Atr,[H|T],Val):- \+fapt(av(Atr,H),_,_),assert_fapt(Atr,[H,fc,Val]),nu_conteaza(Atr,T,Val);nu_conteaza(Atr,T,Val).
+
+nu_conteaza(Atr,[H]):- \+fapt(av(Atr,H),_,_),assert_fapt(Atr,[H]);true.
+nu_conteaza(Atr,[H|T]):- \+fapt(av(Atr,H),_,_),assert_fapt(Atr,[H]),nu_conteaza(Atr,T);nu_conteaza(Atr,T).
 
 citeste_opt(X,Optiuni,Istorie) :-
 append(['('],Optiuni,Opt1),
